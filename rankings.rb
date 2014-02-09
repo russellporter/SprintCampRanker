13928b497cd7a1fe2cd02e17f7bad9f117c8eb39
Russell Porter <contact@russellporter.com>
2014-02-09 11:53:07 -0800

    More work

--- rankings.rb
+++ rankings.rb
@@ -3,30 +3,44 @@ require 'csv'
 require 'time'
 
 class Ranking
+  attr_reader :results
+  attr_reader :time_results
+
   def initialize
     @results = {}
+    @time_results = {}
   end
 
-  def add_result(percent, race_name) 
+  def add_result(percent, race_name, time) 
     @results[race_name] = percent
+    @time_results[race_name] = time
   end
 
   def percent_sum
-    values = @results.values.sort.reverse
-    max_results = values.length
-    if max_results > 4
-      max_results = 4
+    sum = 0
+    included_events.each do |item|
+      sum += item[:result]
+    end
+    sum
+  end
+
+  def included_events
+    results_a = []
+    @results.each { |event, result|
+      results_a << { :event => event, :result => result }
+    }
+    results_a = results_a.sort_by { |item| item[:result] }.reverse
+    max_results = results_a.length
+    result_cap = 2
+    if max_results > result_cap
+      max_results = result_cap
     end
 
     if (max_results == 0)
       return 0
     end
 
-    sum = 0
-    values.take(max_results).each do |value|
-      sum += value
-    end
-    sum
+    results_a.take(max_results)
   end
 
   def points
@@ -42,6 +56,7 @@ class ResultRanker
     @female_competitors = {}
     @known_men = []
     @known_women = []
+    @races = []
     @detector = SexMachine::Detector.new
   end
 
@@ -75,7 +90,7 @@ class ResultRanker
         gender = :male
       end
     end
-    
+
     gender
   end
 
@@ -92,7 +107,8 @@ class ResultRanker
     @known_women = women
   end
 
-  def add_event(name, filename)
+  def add_event(event_name, filename, valid_klass)
+    @races << event_name
     options = { :headers => true, :col_sep => ';', :encoding => Encoding.find("ISO-8859-1") }
     best_male_time = nil
     best_female_time = nil
@@ -107,19 +123,20 @@ class ResultRanker
         #puts "Parse time " + seconds.to_s + ", " + name
       end
       status = row[12]
+      klass = row[17]
       gender = gender_of_competitor(first_name, last_name)
-      if (gender == :male and best_male_time.nil?)
-        best_male_time = seconds
-        puts "Recorded best male time " + seconds.to_s
-      end
 
-      if (gender == :female and best_female_time.nil?)
-        best_female_time = seconds
-        puts "Recorded best female time " + seconds.to_s
-      end
+      if (status == '0' and klass == valid_klass) then
+        if (gender == :male and best_male_time.nil?)
+          best_male_time = seconds
+          puts "Recorded best male time " + seconds.to_s
+        end
 
-      if (status == '0') then
-        #puts name + " " + time
+        if (gender == :female and best_female_time.nil?)
+          best_female_time = seconds
+          puts "Recorded best female time " + seconds.to_s
+        end
+
         ranking = ranking_for_competitor(name, gender)
         percent_time = 0
         if (gender == :male)
@@ -129,12 +146,17 @@ class ResultRanker
         end
 
         #puts "Rank of " + name + " is " + (percent_time * 1000).to_s
-        ranking.add_result(percent_time, name)
+        minutes_t = (seconds / 60).floor
+        seconds_t = (seconds % 60).round.to_s
+        ranking.add_result(percent_time, event_name, minutes_t.to_s + ":" + "%02d" % seconds_t)
+      elsif (status == '0')
+        ranking = ranking_for_competitor(name, gender)
+        ranking.add_result(0, event_name, "Short course")
       end
     end 
   end
 
-  def build_rankings(gender)
+  def build_rankings(gender, f)
     competitors = nil
     if (gender == :male)
       competitors = @male_competitors
@@ -146,16 +168,56 @@ class ResultRanker
     competitors.each do |competitor, ranking|
       ranks << { :competitor => competitor, :ranking => ranking }
     end
-    
+
     ranks.sort! do |a, b|
       b[:ranking].points <=> a[:ranking].points
     end
 
     n = 1
+    f.puts '<table class="table table-striped">'
+    f.puts '<thead>'
+    f.puts '<tr>'
+    f.puts '<th>#</th>'
+    f.puts '<th>Name</th>'
+    f.puts '<th>Points</th>'
+    @races.each do |race|
+      f.puts '<th>' + race + '</th>'
+    end
+    f.puts '</tr>'
+    f.puts '</thead>'
+    f.puts '<tbody>'
     ranks.each do |rank|
-      puts "#" + n.to_s + ": " + rank[:competitor]+ " with " + rank[:ranking].points.to_s
+      ranking = rank[:ranking]
+      f.puts '<tr>'
+      f.puts '<td>' + n.to_s + '</td>'
+      f.puts '<td>' + rank[:competitor] + "</td>"
+      f.puts '<td>' + '%.2f' % ranking.points + '</td>'
+      @races.each do |race|
+        f.puts '<td>'
+        results = ranking.results
+        included_events = ranking.included_events
+        excluded = true
+        included_events.each { |event|
+          if (event[:event] == race) then
+            excluded = false
+          end
+        }
+        if excluded then
+          f.puts '<del>'
+        end
+        if results.has_key? race
+          f.puts '%.2f' % (results[race] * 1000) + " (" + ranking.time_results[race] + ")"
+        end
+        if excluded then
+          f.puts '</del>'
+        end
+        f.puts '</td>'
+      end
+      f.puts '</tr>'
       n += 1
     end
+    f.puts '</tbody>'
+    f.puts '</table>'
   end
 end
 
@@ -163,9 +225,20 @@ ranker = ResultRanker.new
 men = ['Jiri Krejci', 'Chris Benn', 'Zbynek Cernin', 'Cameron Devine', 'Gudni Karl', 'Oyvind Naess', 'Chris Bullock', 'Roan McMillan', 'Nevin French']
 women = ['Carol Ross', 'Silken Kleer', 'Abra McNair']
 ranker.add_gender_exceptions(men, women)
-ranker.add_event("SI Race #1: Coal Harbour", "coal_harbour.csv")
-puts "Male"
-ranker.build_rankings(:male)
-
-puts "Female"
-ranker.build_rankings(:female)
+ranker.add_event("#1: Coal Harbour", "coal harbour.csv", '1')
+ranker.add_event("#2: Mundy Park", "Mundy Park.csv", '1')
+ranker.add_event("#3: Hume Park Farsta", "farsta.csv", '118')
+File.open('sprint_camp_2014_rankings.html', 'w') do |file|
+  file.puts '<html><head>'
+  file.puts '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">'
+  file.puts '</head><body><div class="container">'
+  file.puts '<header class="page-header">'
+  file.puts "<h1>Sprint Camp 2014 Rankings</h1>"
+  file.puts '</header>'
+  file.puts '<p class="bg-warning" style="padding: 15px; max-width: 450px"><span class="glyphicon glyphicon-question-sign"></span> See a problem? Email <a href="mailto:contact@russellporter.com">contact@russellporter.com</a></p>'
+  file.puts '<h2>Men</h2>'
+  ranker.build_rankings(:male, file)
+  file.puts '<h2>Women</h2>'
+  ranker.build_rankings(:female, file)
+  file.puts '</div></body></html>'
+end
